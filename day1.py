0b181ab2fee27b9c3ae1b259960fc8e84200ec7c
Parvathysubramanian <59096371+Parvathysubramanian@users.noreply.github.com>
2023-02-03 21:13:39 +0530

    Update and rename replit.python to day1.py

---
+++ day1.py
@@ -0,0 +1,3 @@
+print("Hellow World!)
+print(""" Hello World, i am Parvathy Subramanian and new to programing.
+I must say that i am enjoying 100 days of cooding with Replit. I am looking forward to complete it soon.""")
