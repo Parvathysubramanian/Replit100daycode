a08a4f19fd1116b18e19c6aa58a44fc89f9572c7
Parvathy Subramanian <59096371+Parvathysubramanian@users.noreply.github.com>
2023-02-06 03:42:53 +0530

    Create day6.py

---
+++ day6.py
@@ -0,0 +1,10 @@
+print("""MY LOGIN SYSTEM
+         ++++++++++++++++""")
+Username = input("Username >")
+Password = input("Password >")
+if Username == "David" and Password == "totallyNotBald":
+   print("Why hello there",Username, "what a lovely accent you have, you could  have charmed yoir way in here ven without a password")
+elif Username =="Suzan" and Password == "thisIsSuzan":
+  print("Why hello there",Username, "what a lovely accent you have, you could  have charmed yoir way in here ven without a password")
+else:
+   print("Go away")
