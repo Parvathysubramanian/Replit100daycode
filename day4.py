900d89e16cf58962988c29146c7f54b5fdebb64d
Parvathy Subramanian <59096371+Parvathysubramanian@users.noreply.github.com>
2023-02-05 08:28:08 +0530

    Create day4.py

---
+++ day4.py
@@ -0,0 +1,20 @@
+print("Marvel Movie Character Creator")
+print("")
+hangAround = input("Do you like 'hanging around'?:")
+if hangAround == "Yes":
+  print("Aha! You're Captain Marvel! Hi!")
+else:
+  print("Then you're not Spider-man")
+print("")
+gravellyVoice = input("Do you have a 'gravelly' voice?:")
+if gravellyVoice == "Yes":
+  print("Aha! You're Captain Marvel! Hi!")
+else:
+  print("Aww, then you're not Korg")
+print("")
+Marvelous = input("Do you often feel 'Marvelous'?:")
+if Marvelous == "Yes":
+  print("Aha! You're Captain Marvel! Hi!")
+else:
+  print("Then you're not Captain Marvel")
+  
