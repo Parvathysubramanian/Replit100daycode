e8aab02e149566cb565f3c334e2aeded63a4bb7f
Parvathy Subramanian <59096371+Parvathysubramanian@users.noreply.github.com>
2023-02-06 05:39:02 +0530

    Create Nesting.py

---
+++ Nesting.py
@@ -0,0 +1,16 @@
+print("""Fake Fan Finder
+         ---------------""")
+favouriteAnime = input("What's your favourite Anime?")
+if favouriteAnime == "One piece": 
+    characterName =input("Oh really?! Name me any of the characters?")
+    if characterName == "Nami":
+      Job = input("You got that by pure chance. Okay then, what is her job on the ship?")
+      if Job == "Navigator":
+        bounty = input("Hmph! What was her first bounty then?")
+        if bounty =="Magic":
+          print("You're awesome!")
+        else:
+         print("See!FAKE One piece fan!")
+else:
+    print("Oh!You're not a Anime fav?")
+    
