a30902509776c22d04c8292d493fd6f535493c36
Parvathy Subramanian <59096371+Parvathysubramanian@users.noreply.github.com>
2023-02-08 06:16:49 +0530

    Create calculus.py

---
+++ calculus.py
@@ -0,0 +1,14 @@
+adding = 4+3
+print(adding)
+substraction = 4-3
+print(subrstraction)
+multiplication = 3*4
+print(multiplication)
+division =3/4
+print(dividion)
+squared = 3**4
+print(squared)
+modulo = 3%4
+print(modulo) *reminder*
+divisor =3//4 *whole number divisor*
+print(divisor)
