200ee6fa94ca70bb18162eda81a77e5e87bd678a
Parvathy Subramanian <59096371+Parvathysubramanian@users.noreply.github.com>
2023-02-08 10:11:00 +0530

    Create totalseconds.py

---
+++ totalseconds.py
@@ -0,0 +1,8 @@
+year = int(input("Lets check the the total seconds in the year, What is the year?"))
+if year % 4 == 0 and year % 100 and year % 400 ==0:
+  totalLseconds = 366*24*60*60
+  print("The total seconds in a leap year is", totalLseconds)
+      
+else:
+  totalNseconds = 365*24*60*60
+  print("The total seconds in a non leap year is",  totalNseconds)
