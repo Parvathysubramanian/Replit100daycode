a1d2c75ad08ccde6851525247cabcf64bc6d6c5c
Parvathy Subramanian <59096371+Parvathysubramanian@users.noreply.github.com>
2023-02-09 09:00:05 +0530

    Create RockPaperScissors.py

---
+++ RockPaperScissors.py
@@ -0,0 +1,22 @@
+print("E P I C   🪨  🗞️  ✂️    B A T T L E")
+print("Select your move (R, P or S)")
+Player1 = input("Player 1 > ")
+Player2 = input("Player 2 > ")
+if Player1 == "R":
+  Player1 ="Rock"
+elif Player1 =="P":
+  Player1 ="Paper"
+elif Player =="S":
+  Player1 ="Scissors"
+else:
+  print("Try again")
+if Player2 =="R":
+  Player2 ="Rock"
+elif Player2 =="P":
+  Player2 ="Paper"
+elif Player2 =="S":
+  Player2 = "Scissors"
+else:
+  print("Try again")
+    
+print("Player1's", Player1,"is smothered by Player2's",Player2,"!" )
