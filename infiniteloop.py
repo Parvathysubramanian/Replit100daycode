521e8b6687989c225fe82896b36f3a5644ee3b57
Parvathy Subramanian <59096371+Parvathysubramanian@users.noreply.github.com>
2023-02-11 07:31:30 +0530

    Create infiniteloop.py

---
+++ infiniteloop.py
@@ -0,0 +1,37 @@
+exit = "no"
+while exit == "no":
+  animal_sound = input("What animal do you want?")  
+  
+  if animal_sound =="cow" or animal_sound =="Cow":
+    print("A Cow goes Moo")
+  
+  elif animal_sound =="Pig" or animal_sound =="pig":
+   print("A Pig goes Oink")
+
+  elif animal_sound =="Sheep" or animal_sound =="sheep":
+   print("A Sheep goes Baa")
+
+  elif animal_sound =="Duck" or animal_sound =="duck":
+   print("A Duck goes Quack")
+
+  elif animal_sound =="Dog" or animal_sound =="dog":
+    print("A Dog goes Woof")
+
+  elif animal_sound =="Cat" or animal_sound =="cat":
+    print("A Cat goes Meow")
+  
+  else:
+   print("I dont know what that animal sound. Try again")
+
+  exit = input("Do you want to exit?:")
+
+  exit = "no"
+  while exit == "no":
+  
+    animal1 = input ("What animal do you want?")
+    if animal1 == "Lesser Spotted Lemur":
+      print("Umm... the Lesser spotted Lemur goes Awooga?")
+    else:
+      print("Try again")
+    
+    exit = input("Do you want to exit?
