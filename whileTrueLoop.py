411e0c0dbf8055f3d38e87decdfcb042e3b71b0f
Parvathy Subramanian <59096371+Parvathysubramanian@users.noreply.github.com>
2023-02-12 08:37:39 +0530

    Create whileTrueLoop.py

---
+++ whileTrueLoop.py
@@ -0,0 +1,7 @@
+#True means infinite loops
+print("Welcome Bud!")
+while True:
+  name = input("What is your name?")
+  if name == "Paru" or name == "paru":
+    break 
+print("Finally you're here Paru!")
