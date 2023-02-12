620b846c7bc76af3706a511527173e149a730475
Parvathy Subramanian <59096371+Parvathysubramanian@users.noreply.github.com>
2023-02-12 09:17:42 +0530

    Create Loopnumberofattempts.py

---
+++ Loopnumberofattempts.py
@@ -0,0 +1,11 @@
+print("Fill-in the blank Lyrics!")
+print("(type in the blank lyrics. see if you're as cool s me)")
+
+counter = 0
+while True:
+  User_input =input("Never going to _____ you u: ")
+  counter +=1
+  if User_input == "give":
+   break
+
+print("Well done, it only took you",counter,"attempts!")
